138d818acafe6d398a6aa667f30917aa92447750
Jimi Odeyemi <jimode68@gmail.com>
2014-05-04 00:28:06 +0100

    add configuration

---
+++ config/untitled.sh
@@ -0,0 +1,6 @@
+gmail_username: <%= ENV[""GMAIL_USERNAME""] %>
+gmail_password: <%= ENV["GMAIL_PASSWORD"] %> 
+mailchimp_api_key: <%= ENV["aaae2208abbe426a8ff04023369bcedd-us8"] %> 
+mailchimp_list_id: <%= ENV["5d8ff68b0c"] %> 
+domain_name: <%= ENV["DOMAIN_NAME"] %>
+owner_email: <%= ENV["OWNER_EMAIL"] %>
